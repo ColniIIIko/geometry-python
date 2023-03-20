2b05638c0e9e0c44fc85ec5a4359e73675317223
StanEkso <90618842+StanEkso@users.noreply.github.com>
2023-03-20 17:50:27 +0300

    feat: clipSegment

---
+++ lab8/main.py
@@ -0,0 +1,97 @@
+import math
+from shared.point import Point
+from shared.vector import Vector
+from shared.segment import Segment
+from shared.colors import COLORS
+from shared.drawers import drawPoint
+import shared.random as rand_utils
+import pygame
+from shared.drawers import drawPoint, drawLine, drawPolygon
+from shared.convex_polygon import ConvexPolygon
+
+
+def clipSegment(segment: Segment, polygon: ConvexPolygon):
+    points = polygon.points
+    n = len(points)
+    minimalParameter = 0
+    maximumParameter = 1
+    for i in range(n):
+        polygonEdge = Segment(points[i], points[(i + 1) % n])
+        parameter = getSegmentIntersectionParameter(polygonEdge, segment)
+        if parameter is None:
+            continue
+        if getPointClassification(segment, polygonEdge) == 'ПВ':
+            minimalParameter = max(minimalParameter, parameter)
+            pass
+        else:
+            maximumParameter = min(maximumParameter, parameter)
+            pass
+    newStart = segment.start * (1 - minimalParameter) + \
+        segment.end * minimalParameter
+    newEnd = segment.start * (1 - maximumParameter) + \
+        segment.end * maximumParameter
+    newSegment = Segment(
+        newStart, newEnd
+    )
+    return newSegment
+
+
+def isAimedAt(a: Vector, b: Vector) -> bool:
+    """
+    Определяет, нацелен ли вектор a на b
+    """
+
+
+def getSegmentIntersectionParameter(AB: Segment, CD: Segment) -> float | None:
+    """
+    Ищет параметр (для CD) точки пересечения отрезков AB & CD
+    """
+    if not Segment.isIntersects(AB, CD):
+        return None
+    p1 = AB.start
+    p3 = CD.start
+    p4 = CD.end
+    normalVector = AB.toVector().getLeftHandNormal()
+    return - Vector.scalarProduct(p3 - p1, normalVector) / Vector.scalarProduct(p4 - p3, normalVector)
+
+
+def getPointClassification(AB: Segment, pIpIPlus: Segment):
+    directionalVector = pIpIPlus.toVector()
+    normalVector = Vector(directionalVector.y, -directionalVector.x)
+    product = Vector.scalarProduct(AB.toVector(), normalVector)
+    if (product > 0):
+        return "ПП"
+    elif (product < 0):
+        return "ПВ"
+    return ""
+
+
+if __name__ == "__main__":
+    pygame.init()
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((800, 800))
+    pygame.display.set_caption("Lab 8")
+    screen.fill(COLORS["WHITE"])
+    convexPolygonPoints = [
+        Point(600, 453, "q0"),
+        Point(414, 497, "q1"),
+        Point(196, 446, "q2"),
+        Point(62, 285, "q3"),
+        Point(180, 84, "q4"),
+        Point(600, 100, "q5"),
+        Point(650, 360, "q6"),
+    ]
+
+    convexPolygon = ConvexPolygon(convexPolygonPoints)
+    drawPolygon(screen, convexPolygonPoints, COLORS["BLACK"])
+    clipper = Segment(Point(100, 200), Point(700, 600))
+    drawLine(screen, clipper.start, clipper.end, COLORS["BLACK"])
+    SEGMENT = clipSegment(clipper, convexPolygon)
+    drawLine(screen, SEGMENT.start, SEGMENT.end, COLORS["RED"])
+    pygame.display.update()
+    FPS = 2
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
---
+++ lab8/shared/convex_polygon.py
@@ -0,0 +1,26 @@
+from .polygon import Polygon
+from .point import Point
+from .segment import Segment
+
+
+class ConvexPolygon(Polygon):
+    def contains(self, point: Point):
+        """
+        Бинарный тест принадлежности точки (для выпуклого многоугольника)
+        """
+        n = len(self.points)
+        if -1 * Segment(self.points[0], self.points[1]).determinePosition(point) < 0 or -1 * Segment(self.points[0], self.points[n-1]).determinePosition(point) > 0:
+            return False
+
+        # TODO: Проверить, есть ли необходимость начинать индексировать с 0
+        start, end = 1, n - 1
+        while end - start > 1:
+            center = (start + end) // 2
+            if -1 * Segment(self.points[0], self.points[center]).determinePosition(point) < 0:
+                end = center
+            else:
+                start = center
+        edge = Segment(self.points[start], self.points[end])
+        # Лежат ли точки p1 и point по одну сторону от полученной стороны
+        return edge.determinePosition(self.points[0]) * edge.determinePosition(point) > 0
+    pass
